d6bf4f5d9f64af355ee9cb9e0a38f36b44de3a6f
poppykode <ngonimug@gmail.com>
2025-02-16 18:25:18 +0200

    added getters and setters to blog and comment models

--- src/main/java/zw/co/fiscit/model/Blog.java
+++ src/main/java/zw/co/fiscit/model/Blog.java
@@ -6,5 +6,38 @@ public class Blog {
     private String content;
     private String postedBy;
 
+    public Blog() {
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
 }
--- src/main/java/zw/co/fiscit/model/Comment.java
+++ src/main/java/zw/co/fiscit/model/Comment.java
@@ -4,4 +4,31 @@ public class Comment {
     private Long id;
     private Long postId;
     private String content;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
